ba46bd7532d019a1b58ccf48a3476fe76984ae3f
YashaswiniNelakurthi <108202656+YashaswiniNelakurthi@users.noreply.github.com>
2022-06-25 22:19:11 -0400

    Add files via upload
    first commit

---
+++ src/components/signup.component.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+export default class SignUp extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Student Club Registration</h3>
+
+                <div className="form-group">
+                    <label>First name</label>
+                    <input type="text" className="form-control" placeholder="First name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Last name</label>
+                    <input type="text" className="form-control" placeholder="Last name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <div className="form-group">
+                    <label>Club name</label>
+                    <select  className="form-control">
+                        <option value={0}>--Select--</option>
+                        <option value={1}>Robotics Club </option>
+                        <option value={2}>Computer Club</option>
+                        <option value={3}>Science Club</option>
+                        <option value={4}>Machine Learning Club</option>
+                    </select>
+                </div>
+
+                <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
+                <p className="forgot-password text-right">
+                    Already registered <a href="#">log in?</a>
+                </p>
+            </form>
+        );
+    }
+}
